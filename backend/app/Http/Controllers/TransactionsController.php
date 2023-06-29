152651223ce1d406cadb751b6ba3c345bf465408
Mohamed G <gemy.dev@gmail.com>
2023-06-29 23:32:45 +0300

    Small fix

--- backend/app/Http/Controllers/TransactionsController.php
+++ backend/app/Http/Controllers/TransactionsController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCases\Contracts\RetrieveDataInterface;
 use App\Http\Resources\TransactionResource;
-use App\UseCases\AggregatedPayments\RetrieveData;
 use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
     public function users(
         Request $request,
-        RetrieveData $useCase
+        RetrieveDataInterface $useCase
     ) {
         $transactions = $useCase->execute(
             $request->all()
