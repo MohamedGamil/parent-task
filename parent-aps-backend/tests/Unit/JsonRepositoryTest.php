f76dfa2e1e6d1d2a2cfbec56ec72d6af9ecf6e57
Mohamed G <gemy.dev@gmail.com>
2023-06-29 09:33:33 +0300

    Add some unit tests

--- parent-aps-backend/tests/Unit/JsonRepositoryTest.php
+++ parent-aps-backend/tests/Unit/JsonRepositoryTest.php
@@ -8,16 +8,25 @@ use Tests\TestCase;
 
 class JsonRepositoryTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_json_repository_x_instance_of_json_repo(): void
+    public function test_json_repo_instance_of_json_repo(): void
     {
         /** @var DataProviderXRepository $repox */
         $repox = app()->make(DataProviderXRepository::class);
 
         $this->assertTrue(
             $repox instanceof JsonRepository
+        );
+    }
+
+    public function test_json_repo_query_data(): void
+    {
+        /** @var DataProviderXRepository $repox */
+        $repox = app()->make(DataProviderXRepository::class);
+
+        $this->assertGreaterThan(
+            0,
+            $repox->query()->count(),
+            "DataProviderX repository all method should return records."
         );
     }
 }
